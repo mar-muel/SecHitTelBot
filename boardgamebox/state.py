bf6c08d41289d6996aa18c9e6d28ce5701cf92fe
Martin Muller <martin@poolside.ai>
2026-04-03 00:34:01 +0200

    add typing for state

--- boardgamebox/state.py
+++ boardgamebox/state.py
@@ -4,16 +4,16 @@ from game_types import Policy
 
 class State:
     def __init__(self):
-        self.liberal_track = 0
-        self.fascist_track = 0
-        self.failed_votes = 0
+        self.liberal_track: int = 0
+        self.fascist_track: int = 0
+        self.failed_votes: int = 0
         self.president: Player | None = None
         self.nominated_president: Player | None = None
         self.nominated_chancellor: Player | None = None
         self.chosen_president: Player | None = None
         self.chancellor: Player | None = None
         self.drawn_policies: list[Policy] = []
-        self.player_counter = 0
-        self.veto_refused = False
+        self.player_counter: int = 0
+        self.veto_refused: bool = False
         self.not_hitlers: list[Player] = []
         self.inspected_players: list[Player] = []
